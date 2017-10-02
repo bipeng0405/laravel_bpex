aff63217e12aec34b5f5c287dab92731683e7b46
bipeng0405@163.com <bipeng0405@163.com>
2017-10-02 15:04:32 +0800

    增加测试文件

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 by BPEX.CC</div>
+                <div class="title">Laravel 5 by BPEX.CC.</div>
             </div>
         </div>
     </body>
